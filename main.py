04337cf8c372c6e47e5bb6b4dfb4f10eb48c0a06
Abhishek Chauhan <bbiilluu9211@gmail.com>
2020-11-20 17:16:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+#taking users input
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#randomnized the list which user chosse
+random_letter = random.sample(letters,nr_letters)
+random_numbers = random.sample(numbers,nr_numbers)
+random_symbols = random.sample(symbols,nr_symbols)
+
+#merge all three list to make one password
+mixed_pass = random_letter + random_numbers + random_symbols
+
+#shuffling the list to make stronge password every time
+random.shuffle(mixed_pass)
+
+#converting list into string
+password = ""
+for char in mixed_pass:
+  password += char
+print(f"Your strong password is: {password}")
+
